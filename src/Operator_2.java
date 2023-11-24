1cd24a4e24f44a76804c5b3db55b12ace32ebef1
Vol35 <140203124+Vol35@users.noreply.github.com>
2023-11-24 21:33:38 +0300

    Operator_2

---
+++ src/Operator_2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Operator_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите первое число:  ");
+            double num1 = sc.nextDouble();
+        System.out.print("Введите второе число:  ");
+            double num2 = sc.nextDouble();
+        System.out.print("Введите символ (+, -, *, /): ");
+        char symbol = sc.next ().charAt (0);
+            double answer;
+                switch (symbol){
+                    case '+': answer = num1 + num2;
+                        System.out.printf("Результат = " + String.valueOf(answer));
+                        break;
+                    case '-': answer = num1 - num2;
+                        System.out.printf("Результат = " + String.valueOf(answer));
+                        break;
+                    case '*': answer = num1 * num2;
+                        System.out.printf("Результат = " + String.valueOf(answer));
+                        break;
+                    case '/': answer = num1 / num2;
+                        System.out.printf("Результат = " + String.valueOf(answer));
+                        break;
+                    default:  System.out.printf("Ошибка");
+                }
+    }
+
+}
