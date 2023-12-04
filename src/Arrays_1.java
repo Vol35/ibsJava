7689209bfcf25f8c7cb503e62dacd3d89c135d8f
Vol35 <140203124+Vol35@users.noreply.github.com>
2023-12-04 14:05:31 +0300

    Operator_3

---
+++ src/Arrays_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Arrays_1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+        int z = 4;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите длинну массива: ");
+        int length = in.nextInt();
+        int array[] = new int[length];
+        System.out.println("Введите элементы массива: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = in.nextInt();
+            if (array[i] == x | array[i] == y | array[i] == z )
+                System.out.println("Данное значение имеется в константах");
+
+        }
+    }
+}
