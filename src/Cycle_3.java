d85c76d11e5247cc302baaf8c6ca797d6515fb79
Vol35 <140203124+Vol35@users.noreply.github.com>
2023-12-04 15:58:07 +0300

    Cycle_2

---
+++ src/Cycle_3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Cycle_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Шаг 1: Ввод размера массива
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        // Создаем массив указанного размера
+        double[] array = new double[size];
+
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextDouble();
+        }
+
+        double sum = 0;
+        for (double num : array) {
+            sum += num;
+        }
+        double average = sum / size;
+
+        System.out.println("Среднее арифметическое элементов массива: " + average);
+        System.out.println("Массив с элементами, умноженными на среднее арифметическое:");
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i] * average);
+        }
+    }
+}
