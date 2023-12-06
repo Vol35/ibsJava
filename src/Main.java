f7efc063c9ffc14833f2c18b9a9f556128125e42
Vol35 <140203124+Vol35@users.noreply.github.com>
2023-12-06 11:28:23 +0300

    Main_1 правка суммы и ср.числа

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
-
     public static void main(String[] args) {
-    int[] num = {1,2,3,4,5};// 0 1 2 3 4
-        int firstNum = num [0];
-        num [0] = num[num.length - 1];
-        num[num.length - 1] = firstNum;
+    int[] array = {1,2,3,4,5};// 0 1 2 3 4
+        int firstNum = array [0];
+
         // Смена первого и последнего элемента
-        System.out.println(num[0] + num.length/2);
+        array [0] = array[array.length - 1];
+        array[array.length - 1] = firstNum;
+
+        int middleNum = array.length / 2;
+        int sum = array[0] + array[middleNum];
+        System.out.println("Результат суммы: " + sum);
     }
 }
